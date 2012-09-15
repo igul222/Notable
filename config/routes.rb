c5f3f0fe961e160fbc9af9e06e0fe5020cfd2625
Zainul Shah <zain@quad2-0991.wireless-resnet.upenn.edu>
2012-09-15 12:28:40 -0400

    Log out button!

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Pennapp::Application.routes.draw do
 
   root :to => "home#index"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  
+  devise_scope :user do
+    get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
+  end
   resources :lectures do
     resources :notes
   end
