a7cf3712b1586f97c5a14c1a4b8e3f0a2ee0b886
Ishaan Gulrajani <igul222@gmail.com>
2012-09-15 01:45:44 -0400

    Implement Facebook auth with Devise (TODO: add FB API keys)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,10 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  config.omniauth :facebook, "APP_ID", "APP_SECRET",
+    {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}} 
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
