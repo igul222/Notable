3e68490defe47c4105df04a44dd37da0563be7af
Ishaan Gulrajani <igul222@gmail.com>
2012-09-15 02:57:40 -0400

    Fix omniauth callbacks controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Pennapp::Application.routes.draw do
   devise_for :users
 
   root :to => "home#index"
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
