97f47f9a2146221e3ce63587da060fc5450d8970
Ishaan Gulrajani <igul222@gmail.com>
2012-09-15 01:47:21 -0400

    Add FB app ID and secret

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,7 +2,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
 
-  config.omniauth :facebook, "APP_ID", "APP_SECRET",
+  config.omniauth :facebook, "115369605280693", "1d6a704d12e746d150e5f70936a34e69",
     {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}} 
 
   # ==> Mailer Configuration
