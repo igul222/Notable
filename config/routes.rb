32076e0ef8e5c5ba4e078885fe6967d781fbe3a5
Ishaan Gulrajani <igul222@gmail.com>
2012-09-15 03:09:06 -0400

    Remove auto-generated devise_for line in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Pennapp::Application.routes.draw do
 
-  devise_for :users
-
   root :to => "home#index"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
